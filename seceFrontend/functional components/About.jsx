f6268d19f80134ad7f939f68a36e9fb07c7a334d
Agadeeshwaran <agadeeshwarand@gmail.com>
2025-01-22 10:24:48 +0530

    Day 8

--- seceFrontend/functional components/About.jsx
+++ seceFrontend/functional components/About.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+// eslint-disable-next-line no-unused-vars
 const About=()=>{
+    var[text,setText]=useState("hello")
+    // eslint-disable-next-line no-unused-vars
+    function handleText(){
+        setText()
+    }
     return(
         <div>
-            <h2>This is About the Component...</h2>
+            <h2>This is About Component.</h2>
+            <h2>TextArea</h2>
+            <textarea value={text}/>
+            <textarea value={text} onChange={()=>setText()}/>
+            <textarea value={text} onChange={(event)=>setText(event.target.value)}/>
+            <p>text is {text}</p>
         </div>
     )
 }
-export default About;
