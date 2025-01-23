a5e0e5472aa538f7a9282724a81de1f2b0b6623e
Agadeeshwaran <agadeeshwarand@gmail.com>
2025-01-23 08:55:32 +0530

    Day 9 FN and AN

---
+++ seceFrontend/functional components/card.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import Navbar from "./Navbar";
+const Card = ({ title, description }) => {
+    return (
+      <div>
+      <Navbar />
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <div style={{ border: "1px solid #ccc", padding: "15px", borderRadius: "5px", margin: "10px" }}>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      </div>
+    );
+  &rbrace;;
+  
+  export default Card;
+    </div>
+  );
+};
+export default Card;
